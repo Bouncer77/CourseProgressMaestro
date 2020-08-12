f9fbf674e497d483ffec30b572d0c92ff8bca855
Ivan Kosenkov <bouncer77rus@gmail.com>
2020-08-12 23:00:43 +0300

    init service-inventory

--- vtb-course-progress-maestro-service-inventory/src/main/java/com/bouncer77/vtb/course/progress/maestro/service/App.java
+++
@@ -1,7 +0,0 @@
-package com.bouncer77.vtb.course.progress.maestro.service;
-
-public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
-    }
-}
